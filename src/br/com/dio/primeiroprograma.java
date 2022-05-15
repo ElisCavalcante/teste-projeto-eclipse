734363c3ad36b1fc95f53c576a9a0d437ce4a0ec
ElisCavalcante <elisabeth.rsc@gmail.com>
2022-05-14 22:04:18 -0300

    primeiro commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,29 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+	
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a= 2;
+		int b=5;
+		System.out.println("hello world" +(a+b));*/
+		
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+	
+}
